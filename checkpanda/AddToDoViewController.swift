4c886bc650be88aa681214915bfd0db78e9dd97a
Unknown <takagi@iplab.cs.tsukuba.ac.jp>
2018-03-29 01:34:35 +0900

    FormViewの作成(途中)

--- checkpanda/AddToDoViewController.swift
+++ checkpanda/AddToDoViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import Eureka
 
-class AddToDoViewController: UIViewController {
+class AddToDoViewController: FormViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        initForm()
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+
+    func initForm() {
+        form +++ Section("")
+                <<< TextRow() { row in
+                    row.title = "タイトル"
+                }
+            +++ Section("")
+                <<< DateTimeRow() { row in
+                    row.title = "期限"
+                    row.minimumDate = Date()
+                }
+                <<< CheckRow() { row in
+                    row.title = "繰り返し"
+                }
+                <<< PickerInlineRow<String>() { row in
+                    row.title = "カテゴリー"
+                    row.options = ["カテゴリー1", "カテゴリー2"] // TODO fix カテゴリー
+                }
+                <<< PickerInlineRow<String>() { row in
+                    row.title = "優先度"
+                    row.options = ["☆☆★", "☆★★", "★★★"] // TODO fix カテゴリー
+                }
     }
 }
