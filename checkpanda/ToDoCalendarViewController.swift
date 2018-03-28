f5a8f4fd998be7741587e05c192024a8370d3d46
Unknown <takagi@iplab.cs.tsukuba.ac.jp>
2018-03-28 20:14:31 +0900

    現在月の表示, ToDoの表示

--- checkpanda/ToDoCalendarViewController.swift
+++ checkpanda/ToDoCalendarViewController.swift
@@ -7,25 +7,51 @@ import UIKit
 import FSCalendar
 import CalculateCalendarLogic
 
-class ToDoCalendarViewController: UIViewController, FSCalendarDataSource, FSCalendarDelegate, FSCalendarDelegateAppearance {
+class ToDoCalendarViewController: UIViewController,
+                                  FSCalendarDataSource, FSCalendarDelegate, FSCalendarDelegateAppearance,
+                                  UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var calendar: FSCalendar!
 
+    @IBOutlet weak var tableView: UITableView!
+
+    @IBOutlet weak var currentMonth: UIBarButtonItem!
+    func setCurrentMonthTitle(_ date: Date) {
+        currentMonth.title = monthDateFormatter.string(from: date)
+    }
+
+    
+    var toDoList: [ToDo] = [ToDo(t: "test1")]
+
+    let cellIdentifier = "toDoCell"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let header = calendar.calendarHeaderView.collectionViewLayout.
-//        print(header)
-//        calendar.calendarHeaderView
+        let nib = UINib(nibName: "ToDoTableViewCell", bundle: nil)
+        self.tableView.register(nib, forCellReuseIdentifier: cellIdentifier)
+
+        initCalendar()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
+    func initCalendar() {
+        calendar.appearance.headerMinimumDissolvedAlpha = 1
+        setCurrentMonthTitle(calendar.currentPage)
+    }
+
     fileprivate let gregorian: Calendar = Calendar(identifier: .gregorian)
     fileprivate lazy var dataFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
+
+    fileprivate lazy var monthDateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM"
         return formatter
     }()
 
@@ -41,7 +67,7 @@ class ToDoCalendarViewController: UIViewController, FSCalendarDataSource, FSCale
         return holiday.judgeJapaneseHoliday(year: year, month: month, day: day)
     }
 
-    func getDay(_ date: Date) -> (Int, Int, Int) {
+    func getDay(_ date: Date) -> (year: Int, month: Int, day: Int) {
         let tmpCalendar = Calendar(identifier: .gregorian)
 
         let year = tmpCalendar.component(.year, from: date)
@@ -64,22 +90,28 @@ class ToDoCalendarViewController: UIViewController, FSCalendarDataSource, FSCale
         let weekday = self.getWeekIdx(date)
         if weekday == 1 {
             return UIColor.red
-        }else if weekday == 7 {
+        } else if weekday == 7 {
             return UIColor.blue
         }
 
         return nil
     }
-    
-
 
     func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
         print(getDay(date))
     }
 
-    func calendar(_ calendar: FSCalendar, prepareHeaderView: UIView, date: Date) {
-
-        print("hoge")
+    func calendarCurrentPageDidChange(_ calendar: FSCalendar) {
+        setCurrentMonthTitle(calendar.currentPage)
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoList.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as! ToDoTableViewCell
+        cell.setCell(todo: toDoList[indexPath.row])
+        return cell
+    }
 }
